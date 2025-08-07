80be83d175fe06855e813cc26a60c06776359bdd
Hari2500 <harikrishnan252000@gmail.com>
2025-08-07 20:08:47 +0530

    Factorial and Palindrome Completed

--- Final/src/com/Factorial.java
+++ Final/src/com/Factorial.java
@@ -1,8 +1,29 @@
 package com;
 
+import java.util.Scanner;
+
 public class Factorial {
+	
+	public static long palindromeOrNot(int num) {
+		long res = 1;
+		if (num < 0) {
+			System.out.println("Factorial is not defined for negative numbers");
+		} else {
+			for (int i = 1; i <= num; i++) {
+				res = res * i;
+			}
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter non-negative number to find factorial: ");
+		int num = sc.nextInt();
+		long res = palindromeOrNot(num);
+		System.out.println("The factorial for the given number " + num + " is: " + res);
+		sc.close();
 		
 	}
 
--- Final/src/com/Palindrome.java
+++ Final/src/com/Palindrome.java
@@ -1,9 +1,35 @@
 package com;
 
+import java.util.Scanner;
+
 public class Palindrome {
 
+	public static boolean checkPalindrome(int num) {
+		int ori = num;
+		int rev = 0;
+		while(num>0) {
+			int rem = num%10;
+			rev = rev*10 + rem;
+			num /= 10;
+		}
+		if(rev == ori)
+			return true;
+		else
+			return false;
+	}
+
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number to check it is palindrome or not: ");
+		int num = sc.nextInt();
+		boolean res =checkPalindrome(num);
 		
+		if(res)
+			System.out.println("The given number " + num + " is palindrome.");
+		else
+			System.out.println("The given number " + num + " is not a palindrome.");
+		
+		sc.close();
 	}
 
 }
