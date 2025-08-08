7483ae5175f7542dc9d5d2933f659699e90e77e8
Hari2500 <harikrishnan252000@gmail.com>
2025-08-08 11:25:22 +0530

    Left triangle

---
+++ Final/src/pattern/LeftTriangle.java
@@ -0,0 +1,31 @@
+package pattern;
+
+public class LeftTriangle {
+
+	static void leftT() {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	static void leftT1() {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j <= i + 1; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		// Left Triangle
+		leftT();
+		leftT1();
+	}
+
+}
