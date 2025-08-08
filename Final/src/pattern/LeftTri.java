65334b1266f39c3917005c7bad01da6319ae3cfb
Hari2500 <harikrishnan252000@gmail.com>
2025-08-08 13:35:29 +0530

    Left triangle

--- Final/src/pattern/LeftTri.java
+++ Final/src/pattern/LeftTri.java
@@ -5,8 +5,11 @@ public class LeftTri {
 	static void rightT() {
 		int n = 5;
 		for(int i=1; i<=n; i++) {
-			for(int j = i; j<=n; j++) {
-				System.out.print("*");
+			for(int j = i; j<=n;j++) {
+				System.out.print("  ");
+			}
+			for(int j = 1; j<=i;j++) {
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
